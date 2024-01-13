a1b815e6b1f51b0e7ece869ac6211e15679fbc14
Ikrash Bakhsh <153372614+Ikrash-Bakhsh@users.noreply.github.com>
2024-01-13 22:04:32 +0500

    Add files via upload

---
+++ Task 11.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,avg;
+	cout<<"Enter first number = ";
+	cin>>a;
+	cout<<"Enter second number = ";
+	cin>>b;
+	cout<<"Enter third number = ";
+	cin>>c;
+	avg=(a+b+c)/3;
+	cout<<"Average of number is = "<<avg;
+	
+	return 0;
+}
